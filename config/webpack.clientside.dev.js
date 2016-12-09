e4041330740a0fd0783487ffc5f696644a8d8a6d
Adrien Remacle <builldog@MacBook-Pro-de-Adrien.local>
2016-12-09 10:04:43 +0100

    entry point

--- config/webpack.clientside.dev.js
+++ config/webpack.clientside.dev.js
@@ -12,17 +12,21 @@ var webpackDevConfig = {
 	//Fichier d"entrée où toutes les dépendances et ressources à inclure
 	//seront cherchées récursivement:
 	entry: [
-		"webpack-dev-server/client?http://"+HMR+":"+port, //WebpackDevServer host et port
-		"webpack/hot/only-dev-server", //"only" permet d"empêcher le rechargement lors d"erreurs de syntaxes
-		//fichier d"entrée principale de notre code source (client et non client.jsx comme le dossier
-		//contient index.jsx donc automatiquement loadé lorsque l"on spécifie le dossier):
-		APP_DIR + "/client"
+		bundle: [
+			"webpack-dev-server/client?http://"+HMR+":"+port, //WebpackDevServer host et port
+			"webpack/hot/only-dev-server", //"only" permet d"empêcher le rechargement lors d"erreurs de syntaxes
+			//fichier d"entrée principale de notre code source (client et non client.jsx comme le dossier
+			//contient index.jsx donc automatiquement loadé lorsque l"on spécifie le dossier):
+			APP_DIR + "/client"
+		],
+		presentation: APP_DIR + "/presentation",
+		"slide-preview": APP_DIR + "/slide-preview"
 	],
 	//Inutile de spécifier target: "web" car par défault
 	//(web pour compiler pour une utilisation sur un environnement browser-like):
 	//target: "web",
 	output: {
-		filename: "client.bundle.js",
+		filename: "[name].js",
 		//path spécifie le dossier de destination des fichiers loadés et/ou bundlés (en chemin absolu):
 		path: BUILD_DIR,
 		//publicPath spécifie l"url publique des ressources bundlées/loadées: lorsqu"un require est rencontré, son URL de sortie sera composé
